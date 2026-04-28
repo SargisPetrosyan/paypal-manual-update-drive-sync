11b794745dced0fbee3aad8a40806e3b7dbb2048
Sargis <saqomax@gmail.com>
2026-04-28 12:46:12 +0200

    git logging file with recovery

--- app/core/logging.py
+++ app/core/logging.py
@@ -6,22 +6,32 @@ Configures log file location, format, and logging levels for the application.
 import logging
 from datetime   import datetime
 from pytz import timezone
+from logging.handlers import RotatingFileHandler
+
 
 from app.constants import SWEDEN_TIMEZONE_NAME
 
 
 def setup_logger() -> None:
+    filename="app/logs/drive_updater_logs.log"
+
     logging.basicConfig(
-        filename="app/logs/drive_updater_logs.log",
+        filename=filename,
         format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
         filemode="a",
         force=True,
     )
     logging.Formatter.converter = lambda *args: datetime.now(tz=timezone(SWEDEN_TIMEZONE_NAME)).timetuple()
 
+    my_handler = RotatingFileHandler(
+        filename=filename, mode='a', 
+        maxBytes=2*1024*1024, 
+        backupCount=5, encoding=None,)
+    
     # Creating an object
     logger: logging.Logger = logging.getLogger()
 
+    logger.addHandler(my_handler)
 
     # Setting the threshold of logger to DEBUG
     logger.setLevel(level=logging.INFO)
